69f224908efd30c2c251ebd07ac9810c39a6a4c2
ashi-psn <psn.arcz.vtzf@gmail.com>
2022-09-15 09:27:10 +0900

    first commit

---
+++ SwiftUIToDoExample/ToDoCreateView.swift
@@ -0,0 +1,20 @@
+//
+//  ToDoCreateView.swift
+//  SwiftUIToDoExample
+//
+//  Created by 葦沢尚也 on 2022/09/15.
+//
+
+import SwiftUI
+
+struct ToDoCreateView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct ToDoCreateView_Previews: PreviewProvider {
+    static var previews: some View {
+        ToDoCreateView()
+    }
+}
